18bc6734d4fc8b5a941d76b4e193188dcbca78d5
Luis Castilla <159273408+luiscastillaPro@users.noreply.github.com>
2024-10-25 15:35:24 +0000

    se hacen algunos ajustes

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -16,34 +16,61 @@ const Semaforo = () => {
 	const addPurple = () => {
 		setColor("purple");
 	};
+
 	return (
-		<div className='contenedor'>
-			<div className='poste'></div>
-			<div className="semaforo">
-				<div
-					className={`foco red ${color === "red" ? "activo-red" : ""}`}
-					onClick={() => setColor("red")}
-				></div>
-				<div
-					className={`foco yellow ${color === "yellow" ? "activo-yellow" : ""}`}
-					onClick={() => setColor("yellow")}
-				></div>
-				<div
-					className={`foco green ${color === "green" ? "activo-green" : ""}`}
-					onClick={() => setColor("green")}
-				></div>
-				{color === "purple" && (
+		<div className='separar'>
+			<div className='contenedor'>
+				<div className='poste'></div>
+				<div className="semaforo">
 					<div
-						className={`foco purple ${color === "purple" ? "activo-purple" : ""}`}
-						onClick={() => setColor("purple")}
+						className={`foco red ${color === "red" ? "activo-red" : ""}`}
+						onClick={() => setColor("red")}
 					></div>
-				)}
+					<div
+						className={`foco yellow ${color === "yellow" ? "activo-yellow" : ""}`}
+						onClick={() => setColor("yellow")}
+					></div>
+					<div
+						className={`foco green ${color === "green" ? "activo-green" : ""}`}
+						onClick={() => setColor("green")}
+					></div>
+					{color === "purple" && (
+						<div
+							className={`foco purple ${color === "purple" ? "activo-purple" : ""}`}
+							onClick={() => setColor("purple")}
+						></div>
+					)}
+				</div>
+				<div className='container-boton'>
+					<button onClick={cambiarColor} className='boton btn btn-primary'>Cambiar Color</button>
+					<button onClick={addPurple} className='boton btn btn-primary'>Agregar morado</button>
+				</div>
 			</div>
-			<div className='container-boton'>
-			<button onClick={cambiarColor} className='boton btn btn-primary'>Cambiar Color</button>
-			<button onClick={addPurple} className='boton btn btn-primary'>Agregar morado</button>
+			<div className='contenedor2'>
+				<div className='poste'></div>
+				<div className="semaforo">
+					<div
+						className={`foco red ${color === "red" ? "activo-red" : ""}`}
+						onClick={() => setColor("red")}
+					></div>
+					<div
+						className={`foco yellow ${color === "yellow" ? "activo-yellow" : ""}`}
+						onClick={() => setColor("yellow")}
+					></div>
+					<div
+						className={`foco green ${color === "green" ? "activo-green" : ""}`}
+						onClick={() => setColor("green")}
+					></div>
+					{color === "purple" && (
+						<div
+							className={`foco purple ${color === "purple" ? "activo-purple" : ""}`}
+							onClick={() => setColor("purple")}
+						></div>
+					)}
+				</div>
 			</div>
 		</div>
+
 	);
 };
 
